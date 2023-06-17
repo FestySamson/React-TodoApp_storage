2c2ded97c5b37cd8464bb19ccd37f97e92a69d3d
Samson Festy <samsonfesty.i@gmail.com>
2023-06-17 14:52:46 +0100

    update

--- vite.config.js
+++ vite.config.js
@@ -4,15 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-// vite.config.js
-export default {
-  // ...
   build: {
     rollupOptions: {
       external: ['dexie'],
     },
   },
-};
+})
+
+// vite.config.js
 
